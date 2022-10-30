65b2f3c24205c8e35669bee338e43296ac6c9833
kishorebittu3003 <113458870+kishorebittu3003@users.noreply.github.com>
2022-10-30 23:47:24 +0530

    Update instancecli.sh

--- instancecli.sh
+++ instancecli.sh
@@ -1,187 +1,172 @@
 #!/bin/bash
-#.......................................................
-REGION="us-west-2"
-VPC_CIDR="10.10.0.0/16"
-PUB_SUBNET="10.10.0.0/24"
-SUBNET_AZ_ZONES="us-west-2a"
-DESTINATION_CIDRBLOCK="0.0.0.0/0"
-GROUP_NAME="Jenkins-Sg"
-##DESCRIPTION="forcli"
+AWS_REGION="us-west-2"
+VPC_NAME="My_VPC"
+VPC_CIDR="192.168.0.0/16"
+SUBNET_PUBLIC_CIDR="192.168.0.0/24"
+SUBNET_PUBLIC_AZ="us-west-2b"
+SUBNET_PUBLIC_NAME="pub_subnet"
+DESTINATION_CIDR_BLOCK="0.0.0.0/0"
+SUBNET_PRIVATE_CIDR="192.168.1.0/24"
 PROTOCOL="tcp"
-###PORT_FOR_SSH="22"
-##PORT_FOR_HTTP="80"
-##PORT_FOR_HTTPS="443"
-##PORT_WEB_SERVERS_WORKING="8080"
+PORT_FOR_SSH="22"
+PORT_FOR_HTTP="80"
+PORT_FOR_HTTPS="443"
+##PORT_FOR_WEB="8080"
+SUBNET_PRIVATE_AZ="us-west-2b"
+SUBNET_PRIVATE_NAME="pvt_subnet"
+SEC_NAME="my_sec"
+INSTANCE_NAME="myinstance"
 IMAGE_ID="ami-017fecd1353bcc96e"
-INSTANCE_TYPE="t2.micro"
-KEY_PAIR_NAME="key"
-INSTANCES_COUNT="1"
-#creating all new
-#creating vpc
+TYPE="t2.micro"
+KEY_NAME="key"
+
+
+#
+#==============================================================================
+#   DO NOT MODIFY CODE BELOW
+#==============================================================================
+#
+# Create VPC
 VPC_ID=$(aws ec2 create-vpc \
   --cidr-block $VPC_CIDR \
-  --tag-specification 'ResourceType=vpc,Tags=[{Key=Name,Value=MyVpc}]' \
-  --region $REGION \
   --query 'Vpc.VpcId' \
-  --output text)
-echo VPC_ID = "$VPC_ID"
+  --tag-specification 'ResourceType=vpc,Tags=[{Key=Name,Value=Oregon-Vpc}]' \
+  --output text \
+  --region $AWS_REGION)
+echo Vpc-Id = $VPC_ID
 
-#creating a pub subnet
-SUBNET=$(aws ec2 create-subnet \
+
+# Create Public Subnet
+ec2public=$(aws ec2 create-subnet \
   --vpc-id $VPC_ID \
-  --cidr-block $PUB_SUBNET \
-  --availability-zone $SUBNET_AZ_ZONES \
-  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Pub_Subnet}]' \
+  --cidr-block $SUBNET_PUBLIC_CIDR \
+  --availability-zone $SUBNET_PUBLIC_AZ \
+  --query 'Subnet.Subnet_Id' \
+  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Public-Subnet}]' \
+  --output text \
+  --region $AWS_REGION)
+echo Subnet_id = $ec2public
+
+
+
+# Create Private Subnet
+ec2private=$(aws ec2 create-subnet \
+  --vpc-id $VPC_ID \
+  --cidr-block $SUBNET_PRIVATE_CIDR \
+  --availability-zone $SUBNET_PRIVATE_AZ \
   --query 'Subnet.SubnetId' \
-  --region $REGION \
-  --output text) 
-echo SUBNET = "$SUBNET"
+  --tag-specifications 'ResourceType=subnet,Tags=[{Key=Name,Value=Private-Subnet}]' \
+  --output text \
+  --region $AWS_REGION)
+echo Private-Subnet-Id = $ec2private
 
-#creating internetgateway
-MY_INTERNETGATEWAY=$(aws ec2 create-internet-gateway \
-  --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=internet-gateway}]' \
-  --region $REGION \
+# Create Internet gateway
+Gateway_id=$(aws ec2 create-internet-gateway \
   --query 'InternetGateway.InternetGatewayId' \
-  --output text)
-echo GatewayId = $MY_INTERNETGATEWAY
+  --tag-specifications 'ResourceType=internet-gateway,Tags=[{Key=Name,Value=Oregon-igw}]' \
+  --output text \
+  --region $AWS_REGION)
+echo Internet_gateway_id = $Gateway_id
 
 
-#attaching internetgateway to vpc
+# Attach internetgateway to vpc 
 aws ec2 attach-internet-gateway \
-  --internet-gateway-id $MY_INTERNETGATEWAY \
   --vpc-id $VPC_ID \
-  --region $REGION 
-echo Vpc_Id = $VPC_ID
-echo GatewayId = $MY_INTERNETGATEWAY
+  --internet-gateway-id $Gateway_id \
+  --region $AWS_REGION
+  echo gateway_id = $Gateway_id
 
-
-
-#creating route tables
-RouteTable_Id=$(aws ec2 create-route-table \
+# Create Route Table
+Table_route=$(aws ec2 create-route-table \
   --vpc-id $VPC_ID \
+  --region $AWS_REGION \
   --query 'RouteTable.RouteTableId' \
-  --region $REGION \
+  --tag-specifications "ResourceType=route-table,Tags=[{Key=Name,Value=Jenkins-Rt}]" \
   --output text) 
-echo RouteTable = "$RouteTable_Id"
+  echo $Routetable_id = $Table_route
 
-#creating route to associate to routetable
- aws ec2 create-route \
-  --route-table-id $RouteTable_Id \
-  --destination-cidr-block $DESTINATION_CIDRBLOCK \
-  --gateway-id $MY_INTERNETGATEWAY \
-  --region $REGION \
-  --output text
-echo Routetableid = $RouteTable_Id
+  #create route to routetable
+  Route_routetable=$(aws ec2 create-route \
+   --route-table-id $Table_route \
+   --destination-cidr-block $DESTINATION_CIDR_BLOCK \
+   --gateway-id $Gateway_id \
+   --region $AWS_REGION \
+    --output text) 
+echo Route_id = $Table_route
 
-
-#attching route to subnet
-ATTACHING_ROUTE_TO_SUBNET=$(aws ec2 associate-route-table \
-  --route-table-id $RouteTable_Id \
-  --subnet-id $SUBNET \
-  --query 'AssociationId' \
-  --region $REGION \
-  --output text)
-echo RouteTable_Id = $RouteTable_Id
+# Associate Public Subnet with Route Table
+Route_id=$(aws ec2 associate-route-table  \
+  --subnet-id $ec2private \
+  --route-table-id $Table_route \
+  --output text \
+  --region $AWS_REGION)
+echo pub_route_id = $Route_id
 
 
-#creating_security_group
-Security_GroupId=$(aws ec2 create-security-group \
-  --group-name $GROUP_NAME \
-   --vpc-id $VPC_ID \
-  --description "forcli" \
-  --tag-specifications 'ResourceType=security-group,Tags=[{Key=Name,Value=Jenkins_Security_Group}]' \
-  --region $REGION \
-  --query 'GroupId' \
-  --output text)
- echo Security-GroupId = $Security_GroupId
 
- #inbound_rules_for_security_group port_22
-forssh=$(aws ec2 authorize-security-group-ingress \
-    --group-id $Security_GroupId \
-    --region $REGION \
+# creating security group
+my_security=$(aws ec2 create-security-group \
+--group-name "MySecurityGroup" \
+--description "My security group" \
+--vpc-id $VPC_ID \
+--region $AWS_REGION \
+--output text)
+echo security = $my_security
+
+
+# inbound rules
+Bitturule=$(aws ec2 authorize-security-group-ingress \
+    --group-id  $my_security \
     --protocol $PROTOCOL \
+    --port $PORT_FOR_SSH \
+    --tag-specifications 'ResourceType=security-group-rule,Tags=[{Key=Name,Value=Open_22}]' \
     --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
-    --port "22" \
-    --cidr $DESTINATION_CIDRBLOCK \
-    --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
-    --tag-specifications 'ResourceType=security-group-rule,Tags=[{Key=Name,Value=Open_Ssh}]' \
+    --cidr $DESTINATION_CIDR_BLOCK \
     --output text)
-echo creating_inbound = $forssh
+    echo rule = $Bitturule
+
+ # inbound rules
+  Bitturule_1=$(aws ec2 authorize-security-group-ingress \
+    --group-id  $my_security \
+    --protocol $PROTOCOL \
+    --port $PORT_FOR_HTTP \
+    --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
+    --tag-specifications 'ResourceType=security-group-rule,Tags=[{Key=Name,Value=Open_80}]' \
+    --cidr $DESTINATION_CIDR_BLOCK \
+    --output text)
+    echo rule = $Bitturule_1
+
+    
 
 
- #inbound_rules_for_security_group port_80
- creating_inbound_rules80=$(aws ec2 authorize-security-group-ingress \
-  --group-id $Security_GroupId \
-  --region $REGION \
-  --protocol $PROTOCOL \
-  --port "80" \
-  --cidr $DESTINATION_CIDRBLOCK \
-  --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
-  --output text)
-echo creating_80 = $creating_inbound_rules80
+    # inbound rules
+     sg_rule_3=$(aws ec2 authorize-security-group-ingress \
+    --group-id  $my_security \
+    --protocol $PROTOCOL \
+    --port $PORT_FOR_HTTPS \
+    --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
+    --tag-specifications 'ResourceType=security-group-rule,Tags=[{Key=Name,Value=Open_443}]' \
+    --cidr $DESTINATION_CIDR_BLOCK \
+    --output text)
+     echo rule3 = $sg_rule_3
+     
+  
 
-
- #inbound_rules_for_security_group port_443
- creating_inbound_rules_443=$(aws ec2 authorize-security-group-ingress \
-  --group-id $Security_GroupId \
-  --region $REGION \
-  --protocol $PROTOCOL \
-  --port "443" \
-  --cidr $DESTINATION_CIDRBLOCK \
-  --tag-specifications 'ResourceType=security-group-rule,Tags=[{Key=Name,Value=Open_443}]' \
-  --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
-  --output text)
-echo creating_443 = $creating_inbound_rules_443
-
-
- #inbound_rules_for_security_group port_8080
- creating_inbound_rules_8080=$(aws ec2 authorize-security-group-ingress \
-  --group-id $Security_GroupId \
-  --region $REGION \
-  --protocol $PROTOCOL \
-  --port "8080" \
-  --tag-specifications 'ResourceType=security-group-rule,Tags=[{Key=Name,Value=Open_8080}]' \
-  --cidr $DESTINATION_CIDRBLOCK \
-  --query 'SecurityGroupRules[0].SecurityGroupRuleId' \
-  --output text)
-echo inbound = $creating_inbound_rules_8080
-
-#creating a instance on ubuntu22
-CREATING_INSTANCE=$(aws ec2 run-instances \
+   #creating instance
+  Instance_Id=$(aws ec2 run-instances \
   --image-id $IMAGE_ID \
-  --instance-type $INSTANCE_TYPE \
-  --key-name $KEY_PAIR_NAME \
-  --region $REGION \
-  --subnet-id $SUBNET \
-  --count $INSTANCES_COUNT \
-  --associate-public-ip-address
+  --count "1" \
+  --security-group-ids $my_security \
+  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=Jenkins-Cli-Instance}]' \
+  --subnet-id $ec2private \
+  --instance-type $TYPE \
+  --region $AWS_REGION \
+  --key-name $KEY_NAME \
+  --associate-public-ip-address \
   --query 'Instances[0].InstanceId' \
-  --output text)
- echo instance = $CREATING_INSTANCE
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  --output text) 
+  echo instance = $Instance_Id
  
-
-
-
 
 
 
